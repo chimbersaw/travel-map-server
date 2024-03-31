b8a39f7e0ca2e3db7048b47aa283d249027b9f90
Maksim Sukhodolskii <maksukh@inbox.ru>
2024-03-31 21:37:37 +0200

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.21"
-    kotlin("plugin.spring") version "1.9.21"
-    kotlin("plugin.jpa") version "1.9.21"
-    id("org.springframework.boot") version "3.2.0"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.jpa") version "1.9.23"
+    id("org.springframework.boot") version "3.2.4"
     id("org.liquibase.gradle") version "2.2.1"
 }
 
@@ -20,16 +20,16 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("org.springframework.boot:spring-boot-starter-security:3.2.0")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.0")
-    implementation("org.springframework.boot:spring-boot-starter-web:3.2.0")
-    implementation("org.springframework.boot:spring-boot-starter-mail:3.2.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.4")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
+    implementation("org.springframework.boot:spring-boot-starter-mail:3.2.4")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
 
     implementation("com.auth0:java-jwt:4.4.0")
-    implementation("org.liquibase:liquibase-core:4.25.0")
+    implementation("org.liquibase:liquibase-core:4.27.0")
 
-    runtimeOnly("org.postgresql:postgresql:42.7.0")
+    runtimeOnly("org.postgresql:postgresql:42.7.3")
 }
 
 tasks.withType<KotlinCompile> {
