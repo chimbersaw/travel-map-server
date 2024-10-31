14db929016b2c4513fc9c88092064fc271226195
Maksim Sukhodolskii <maksukh@inbox.ru>
2024-10-31 12:20:37 +0000

    kringe

--- src/main/kotlin/ru/chimchima/travelmap/security/WebSecurityConfig.kt
+++ src/main/kotlin/ru/chimchima/travelmap/security/WebSecurityConfig.kt
@@ -30,7 +30,7 @@ class WebSecurityConfig(
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        val origin = System.getenv("SITE_URL") ?: "http://localhost:3000"
+        val origin = System.getenv("WEBSITE_URL") ?: "http://localhost:3000"
         configuration.allowedOrigins = listOf(origin)
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         configuration.allowedHeaders = listOf("authorization", "content-type", "x-auth-token")
